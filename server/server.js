d8ec8bd8abc52ff575724522bb4a6177d38badc1
Dianne Le <diannefle@gmail.com>
2016-11-28 04:24:06 -0800

    [fix] - change static from app.use to app.get

--- server/server.js
+++ server/server.js
@@ -127,7 +127,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use('/', express.static(path.join(__dirname + 'dist/client')))
+app.get('/', express.static(path.join(__dirname + 'dist/client')))
 
 app.get('/api/auth/github', 
   passport.authenticate('github', { scope: 'email'}), (req, res) => { })
